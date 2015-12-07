5607ecd7423904751dd86d1b09a2b9268e3f73f8
jmschnei <jmschnei@jmschnei-laptop>
2015-12-07 09:30:11 +0100

    Adding NIF read and write staff

---
+++ src/main/java/eu/freme/broker/niftools/NIFUriHelper.java
@@ -0,0 +1,32 @@
+package eu.freme.broker.niftools;
+
+public class NIFUriHelper {
+
+    /**
+     * Transforms the NIF URI
+     * <code>http://example.org/document_1#char=0,120</code> into the document
+     * URI <code>http://example.org/document_1</code>.
+     * 
+     * @param nifUri
+     *            the URI the document has inside the NIF model
+     * @return the documents URI without the character position information
+     */
+    public static String getDocumentUriFromNifUri(String nifUri) {
+        int pos = nifUri.lastIndexOf('#');
+        if (pos > 0) {
+            return nifUri.substring(0, pos);
+        } else {
+            return nifUri;
+        }
+    }
+
+    public static String getNifUri(String documentURI, int start, int end) {
+        StringBuilder uriBuilder = new StringBuilder();
+        uriBuilder.append(documentURI);
+        uriBuilder.append("#char=");
+        uriBuilder.append(start);
+        uriBuilder.append(',');
+        uriBuilder.append(end);
+        return uriBuilder.toString();
+    }
+}
