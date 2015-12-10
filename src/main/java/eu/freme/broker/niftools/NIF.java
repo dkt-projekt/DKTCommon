e28b1b2d209b431925a3b7a525b3922f23fd8d8a
Julian Moreno Schneider <jmschnei@gmail.com>
2015-12-10 16:42:47 +0100

    Added extraction of DocumentURI and Entities from NIFmodel.

--- src/main/java/eu/freme/broker/niftools/NIF.java
+++ src/main/java/eu/freme/broker/niftools/NIF.java
@@ -35,6 +35,7 @@ public class NIF {
     public static final Property confidence = property("confidence");
     public static final Property isString = property("isString");
     public static final Property endIndex = property("endIndex");
+    public static final Property entity = property("entity");
     public static final Property keyword = property("keyword");
     public static final Property referenceContext = property("referenceContext");
     public static final Property topic = property("topic");
--- src/main/java/eu/freme/broker/niftools/NIFReader.java
+++ src/main/java/eu/freme/broker/niftools/NIFReader.java
@@ -2,9 +2,14 @@ package eu.freme.broker.niftools;
 
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.LinkedList;
+import java.util.List;
 
 import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.ResIterator;
 import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.ResourceFactory;
 import com.hp.hpl.jena.rdf.model.Statement;
 import com.hp.hpl.jena.rdf.model.StmtIterator;
 import com.hp.hpl.jena.vocabulary.RDF;
@@ -12,7 +17,7 @@ import com.hp.hpl.jena.vocabulary.RDF;
 //import eu.freme.broker.eopennlp.exceptions.ExternalServiceFailedException;
 
 public class NIFReader {
-
+	
 	public static String extractIsString(Model nifModel){
 		
 		String str = null;
@@ -54,5 +59,36 @@ public class NIFReader {
 		
 		return writer.toString();
 	}
+
+
+	public static String extractDocumentURI(Model nifModel){
+		StmtIterator iter = nifModel.listStatements(null, RDF.type, nifModel.getResource(NIF.Context.getURI()));
+      
+		while(iter.hasNext()){
+			Resource contextRes = iter.nextStatement().getSubject();
+			System.out.println(contextRes.getURI());
+			String uri = contextRes.getURI();
+			return uri.substring(0, uri.indexOf('#'));
+		}
+		throw new eu.freme.broker.exception.BadRequestException("No context/document found.");
+	}
 	
+	public static List<String[]> extractEntities(Model nifModel){
+		List<String[]> list = new LinkedList<String[]>();
+				
+        ResIterator iterEntities = nifModel.listSubjectsWithProperty(NIF.entity);
+        while (iterEntities.hasNext()) {
+            Resource r = iterEntities.nextResource();
+            com.hp.hpl.jena.rdf.model.Statement st = r.getProperty(NIF.entity);
+//            System.out.println("1."+st.getObject().asResource().getURI());
+            com.hp.hpl.jena.rdf.model.Statement st2 = r.getProperty(NIF.anchorOf);
+//            System.out.println("7."+st2.getLiteral().getString());
+            String[] information = {st.getObject().asResource().getURI(),st2.getLiteral().getString()};
+            list.add(information);
+        }
+        if(list.isEmpty()){
+        	return null;
+        }
+		return list;
+	}
 }
